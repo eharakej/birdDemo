315bbbe09aec54b5b5f9cc3b3b44ab97b38248c7
edwardharake <eharakej@umich.edu>
2019-11-17 12:30:57 -0500

    added comment

--- app/src/main/java/com/example/birddemo/MainActivity.java
+++ app/src/main/java/com/example/birddemo/MainActivity.java
@@ -2,10 +2,14 @@ package com.example.birddemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -26,14 +30,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         buttonSubmit.setOnClickListener(this);
         buttonGoTo.setOnClickListener(this);
         //comment
-        
+
     }
 
     @Override
     public void onClick(View view) {
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        final DatabaseReference myRef = database.getReference("Bird");
+        DatabaseReference myRef = database.getReference("Bird");
 
         if (buttonSubmit == view){
 
@@ -44,9 +48,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             Bird myBird = new Bird(submitBirdName, submitZipCode, submitPersonName);
 
+            myRef.push().setValue(myBird);
+
+
+
 
 
         } else if (buttonGoTo == view){
+
+            Intent mainintent = new Intent(this, SearchActivity.class);
+            startActivity(mainintent);
 
         }
 
--- app/src/main/java/com/example/birddemo/SearchActivity.java
+++ app/src/main/java/com/example/birddemo/SearchActivity.java
@@ -1,12 +1,23 @@
 package com.example.birddemo;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import org.w3c.dom.Text;
 
@@ -33,6 +44,61 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
 
     @Override
     public void onClick(View view) {
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("Bird");
+
+        if (buttonSearch == view) {
+
+
+            String temp = editTextZipCodeSearch.getText().toString();
+            int searchZipCode = Integer.parseInt(temp);
+
+
+            myRef.orderByChild("zipcode").equalTo(searchZipCode).addChildEventListener(new ChildEventListener() {
+                @Override
+                public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                    Bird foundBird = dataSnapshot.getValue(Bird.class);
+                    String findBirdName = foundBird.birdname;
+                    String findPersonName = foundBird.personname;
+                    Toast.makeText(SearchActivity.this, "this is working", Toast.LENGTH_SHORT).show();
+
+                    textViewBirdName.setText(findBirdName);
+                    textViewPersonName.setText(findPersonName);
+
+
+                }
+
+                @Override
+                public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                }
+
+                @Override
+                public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+                }
+
+                @Override
+                public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+
+                }
+            });
+
+
+        } else if (buttonGoToReport == view){
+
+            Intent secondaryintent = new Intent(this, MainActivity.class);
+            startActivity(secondaryintent);
+
+        }
 
     }
 }
